3e96f11ee9443d59e98a4043c8c4e52fd74c3653
drumdidam <drumdidam@gmail.com>
2026-05-24 22:44:47 +0200

    First commit

---
+++ app/components/Hero.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full text-center bg-blue-200">
+    <h1>Welcome to MedChat</h1>
+  </div>
+</template>
---
+++ app/components/Nav.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = [
+  {
+    label: "Home",
+    icon: "i-heroicons-home",
+    to: "/",
+  },
+  {
+    label: "Forum",
+    icon: "i-heroicons-chat-bubble-left-right",
+    to: "/forum",
+  },
+  {
+    label: "Search",
+    icon: "i-heroicons-magnifying-glass",
+    to: "/search",
+  },
+  {
+    label: "Sign In",
+    icon: "i-heroicons-arrow-right-end-on-rectangle",
+    to: "/auth/login",
+  },
+  {
+    label: "Sign Up",
+    icon: "i-heroicons-user-plus",
+    to: "/auth/register",
+  },
+];
+</script>
+
+<template>
+  <UNavigationMenu :items="items" />
+</template>
